80e82089db5741d39f96eb30c1a3601ee4a24c4e
Nitu <nitu.agarwal8@gmail.com>
2016-03-25 17:58:07 +0530

    Modifying file to change index

--- firstproject/voteapp/views.py
+++ firstproject/voteapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.template import loader
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -10,10 +9,8 @@ from django.http import HttpResponse
 from .models import Question,Choice
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('voteapp/index.html')
-    context = {'latest_question_list': latest_question_list,}
-    return HttpResponse(template.render(context, request))
+    latest_question_list = Question.objects.order_by('-pub_date')
+    return render(request,'voteapp/index.html', {'latest_question_list': latest_question_list})
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
